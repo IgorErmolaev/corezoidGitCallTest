cad591b984cc903b4cc587b366271ee51681942f
HZ210492EIA <igor.ermolaev@a-bank.com.ua>
2024-02-19 11:52:11 +0200

    T

--- usercode.js
+++ usercode.js
@@ -1,8 +1,10 @@
+const T = require("./test.js");
+
 module.exports = (data) => {
     data.hello = "Hello World!";
     if (data.param == "test") {
-        const test = require("./test.js");
-        data = test.testFunc(data);
+        data = T.testFunc(data);
+        // data = test.testFunc(data);
     } else if (data.param == "test2") {
         const test = require("./test2.js");
         data = test.test2(data);
